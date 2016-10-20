63719f0c908b7a7a814691dc7029bd1e9ec55a81
Eric Johnson <eric3517@icloud.com>
2016-10-20 21:41:02 +0000

    first implementation of actual duplicate detection

--- app/ddet.go
+++ app/ddet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"com.lostbearlabs/ddet"
+	"com.lostbearlabs/ddet/dset"
 	"com.lostbearlabs/ddet/filedb"
 	"fmt"
 	"os"
@@ -10,12 +11,11 @@ import (
 
 func main() {
 	argsWithProg := os.Args
-	if len(argsWithProg) == 3 && argsWithProg[1] == "scan" {
-		doScan(argsWithProg[2])
+	if len(argsWithProg) == 2 {
+		doScan(argsWithProg[1])
 	} else {
 		fmt.Printf("Usage:\n")
-		fmt.Printf("   ddet scan <folder>\n")
-		fmt.Printf("   ddet analyze\n")
+		fmt.Printf("   ddet <folder>\n")
 	}
 }
 
@@ -34,6 +34,12 @@ func doScan(path string) {
 
 	db := filedb.InitDB(dbpath)
 	defer db.Close()
+	
+	scanFiles(path, db)
+	analyzeDuplicates(db, path)
+}
+
+func scanFiles(path string, db *filedb.FileDB) {
 
 	scanner := ddet.MakeScanner(db)
 
@@ -50,4 +56,29 @@ func doScan(path string) {
 
 	scanner.PrintSummary(true)
 	fmt.Printf("COMPLETED SCAN: %s\n", path)
+
+}
+
+func analyzeDuplicates(db *filedb.FileDB, path string) {
+	
+	start := time.Now()
+	ks := dset.New()
+	ks.AddAll(db, path)
+
+	dupKeys := ks.GetDuplicateKeys()
+	fmt.Printf("COMPLETED ANALYSIS, elapsed=%v\n", time.Since(start))
+	
+	if dupKeys == nil || len(dupKeys) == 0 {
+		fmt.Printf("NO DUPLICATES FOUND\n")
+		return
+	}
+
+	for _, key := range dupKeys {
+		entries := ks.GetFileEntries(key)
+		fmt.Printf("Files with MD5 %s and length %d:\n", entries[0].Md5, entries[0].Length)
+		for _, entry := range entries {
+			fmt.Printf("   %s\n", entry.Path)
+		}
+	}
+	
 }
